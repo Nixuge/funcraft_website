9a3a197613d16a2c61be1e8d0960f4f42fa37878
Nixuge <mail@nixuge.me>
2024-07-22 18:34:13 +0200

    Forum index

--- endpoints/forum/forum.py
+++ endpoints/forum/forum.py
@@ -1,8 +1,15 @@
-from flask import render_template
+from flask import render_template, send_file
 from globb import Globb
 
 
-@Globb.app.route('/forum')
-@Globb.app.route('/forum/')
+@Globb.app.route("/forum")
+@Globb.app.route("/forum/")
 def forum():
-    return render_template("forum/todo.html")
+    return render_template("forum/index.html")
+
+# To be moved out whne possible. 
+# There are also other phps to check (see assets/js/xenforo/xenforo.js & ctrl+f ".php")
+@Globb.app.route("/forum/deferred.php", methods=["POST"])
+def deferred_php():
+    return send_file("web/templates/forum/deferred.php")
+
